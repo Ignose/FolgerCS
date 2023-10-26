ce6f11442cf4b55b97ec9a7bf9e1575f24e820b9
Build Script <>
2023-10-26 18:28:03 +0000

    Automated build

--- scripts/FolgerCS/folgercs.js
+++ scripts/FolgerCS/folgercs.js
@@ -17659,9 +17659,6 @@ var WeaponDamageQuest = {
     }
   }]
 };
-function myBaseStat(arg0) {
-  throw new Error("Function not implemented.");
-}
 ;// CONCATENATED MODULE: ./src/tasks/donate.ts
 var donate_templateObject, donate_templateObject2, donate_templateObject3, donate_templateObject4;
 function donate_createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = donate_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
