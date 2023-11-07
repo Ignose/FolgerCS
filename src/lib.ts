a53b64295a631bf73f5e3aab47c10e41cd59f24b
Ignose <ihpares@gmail.com>
2023-11-07 16:43:38 -0500

    Fix skip leafy boy logic

--- src/lib.ts
+++ src/lib.ts
@@ -676,8 +676,13 @@ function checkPrice(thing: Thing): number {
 export function saveLeafyBoys(): boolean {
   if (
     get("_leafMonstersFought") < 3 &&
-    (get("camelSpit") < 15 || get("camelSpit") >= 100 || have($effect`Spit Upon`)) &&
-    (have($item`short stack of pancakes`) || !have($familiar`Shorter-Order Cook`)) &&
+    (get("camelSpit") + camelFightsLeft() * 3.33 < 100 ||
+      get("camelSpit") >= 100 ||
+      have($effect`Spit Upon`) ||
+      !have($familiar`Melodramedary`)) &&
+    (have($item`short stack of pancakes`) ||
+      !have($familiar`Shorter-Order Cook`) ||
+      camelFightsLeft() < 11) &&
     (have($item`robin's egg`) || !have($familiar`Rockin' Robin`)) &&
     (have($item`glob of melted wax`) || !have($familiar`Optimistic Candle`)) &&
     (have($item`burning newspaper`) || !have($familiar`Garbage Fire`))
