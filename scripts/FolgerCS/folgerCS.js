06a55d8f1d70f4fc7a689bbc5c06b98e1d430bd2
Build Script <>
2023-11-13 15:20:29 +0000

    Automated build

--- scripts/FolgerCS/folgerCS.js
+++ scripts/FolgerCS/folgerCS.js
@@ -17300,7 +17300,7 @@ var BoozeDropQuest = {
       (0,external_kolmafia_namespaceObject.restoreHp)(clamp(1000, (0,external_kolmafia_namespaceObject.myMaxhp)() / 2, (0,external_kolmafia_namespaceObject.myMaxhp)()));
       (0,external_kolmafia_namespaceObject.restoreMp)(50);
     },
-    completed: () => !lib_have(template_string_$item(boozedrop_templateObject22 || (boozedrop_templateObject22 = boozedrop_taggedTemplateLiteral(["cosmic bowling ball"])))) || !haveFreeBanish() || $effects(boozedrop_templateObject23 || (boozedrop_templateObject23 = boozedrop_taggedTemplateLiteral(["Cosmic Ball in the Air"]))).some(ef => lib_have(ef)),
+    completed: () => !lib_have(template_string_$item(boozedrop_templateObject22 || (boozedrop_templateObject22 = boozedrop_taggedTemplateLiteral(["cosmic bowling ball"])))) || !haveFreeBanish() || lib_have(template_string_$effect(boozedrop_templateObject23 || (boozedrop_templateObject23 = boozedrop_taggedTemplateLiteral(["Cosmic Ball in the Air"])))),
     do: $location(boozedrop_templateObject24 || (boozedrop_templateObject24 = boozedrop_taggedTemplateLiteral(["The Dire Warren"]))),
     combat: new CombatStrategy().macro(combat_Macro.trySkill(template_string_$skill(boozedrop_templateObject25 || (boozedrop_templateObject25 = boozedrop_taggedTemplateLiteral(["Bowl Straight Up"])))).trySkill(template_string_$skill(boozedrop_templateObject26 || (boozedrop_templateObject26 = boozedrop_taggedTemplateLiteral(["Become a Bat"])))).banish().abort()),
     outfit: {
@@ -17310,7 +17310,7 @@ var BoozeDropQuest = {
       acc2: template_string_$item(boozedrop_templateObject30 || (boozedrop_templateObject30 = boozedrop_taggedTemplateLiteral(["Lil' Doctor\u2122 bag"])))
     },
     limit: {
-      tries: 5
+      tries: 1
     }
   }, {
     name: "Drink Sacramento Wine",
